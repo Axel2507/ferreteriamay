08f6785321d41767366714214b0d665a67429c50
Axel <axeljimenezbravo58@gmail.com>
2025-05-21 14:12:26 -0600

    Visible en la web

--- ferreteriamay/settings.py
+++ ferreteriamay/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = config("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config("DEBUG", default=True, cast=bool)
 
-ALLOWED_HOSTS = config("ALLOWED_HOSTS", default="").split(",")
+ALLOWED_HOSTS = ['ferremay.com', 'www.ferremay.com', '3.84.44.254']
 
 
 # Application definition
@@ -127,7 +127,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'inventario/static/css/estilos.css'),
+    os.path.join(BASE_DIR, 'inventario/static/css/'),
 ]
 
 
