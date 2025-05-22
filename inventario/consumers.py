cd7aeafe21111238b7e836b4fcf267657366e414
Axel <axeljimenezbravo58@gmail.com>
2025-05-21 23:28:16 -0600

    Prueba scanner 8

--- inventario/consumers.py
+++ inventario/consumers.py
@@ -3,15 +3,31 @@ import json
 
 class BarcodeConsumer(AsyncWebsocketConsumer):
     async def connect(self):
-        print("📡 Cliente WebSocket conectado")  # ← este mensaje aparecerá en el log del servidor
+        await self.channel_layer.group_add("barcode_group", self.channel_name)
         await self.accept()
+        print("🔌 Cliente conectado")
 
     async def disconnect(self, close_code):
-        print("❌ Cliente desconectado del WebSocket")
+        await self.channel_layer.group_discard("barcode_group", self.channel_name)
+        print("❌ Cliente desconectado")
 
     async def receive(self, text_data):
-        print("📦 Dato recibido:", text_data)
+        print("📦 Datos recibidos:", text_data)
         data = json.loads(text_data)
         barcode = data.get("barcode")
-        # Enviar el código al navegador conectado (eco)
-        await self.send(json.dumps({"barcode": barcode}))
+        if barcode:
+            # reenviar el código a todos los clientes conectados
+            await self.channel_layer.group_send(
+                "barcode_group",
+                {
+                    "type": "barcode_message",
+                    "barcode": barcode
+                }
+            )
+
+    async def barcode_message(self, event):
+        barcode = event["barcode"]
+        print("📤 Reenviando a navegadores:", barcode)
+        await self.send(text_data=json.dumps({
+            "barcode": barcode
+        }))
