ed2323e7311f0cc80b7d111542c233cb04237178
Axel <axeljimenezbravo58@gmail.com>
2025-05-22 05:25:55 -0600

    prueba 2

--- inventario/urls/categoria_urls.py
+++ inventario/urls/categoria_urls.py
@@ -1,12 +1,9 @@
 from django.urls import path
-from inventario.views.categoria_views import agregar_categoria, listar_categorias, actualizar_categoria,eliminar_categoria,activar_categoria,desactivar_categoria
-
+from inventario.views.categoria_views import agregar_categoria, listar_categorias, actualizar_categoria,eliminar_categoria
 urlpatterns = [
     path('agregar/', agregar_categoria, name='agregar_categoria'),
     path('listar/', listar_categorias, name='listar_categorias'),
     path('actualizar/<str:id>/', actualizar_categoria, name='actualizar_categoria'),
     path('eliminar/<int:id>/', eliminar_categoria, name='eliminar_categoria'),
-    path('activar/<int:id>/', activar_categoria, name='activar_categoria'),
-    path('desactivar/<str:id>/', desactivar_categoria, name='desactivar_categoria')
 
 ]
