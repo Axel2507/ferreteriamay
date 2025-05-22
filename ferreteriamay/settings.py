2cfe7a641edb4531edd8c61089b3d25c6abcda0c
Axel <axeljimenezbravo58@gmail.com>
2025-05-22 09:12:57 -0600

    ruta aws

--- ferreteriamay/settings.py
+++ ferreteriamay/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = config("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config("DEBUG", default=True, cast=bool)
 
-ALLOWED_HOSTS = ['ferremay.com', 'www.ferremay.com', '52.87.191.168']
+ALLOWED_HOSTS = ['ferremay.com', 'www.ferremay.com', '18.212.16.28']
 
 
 # Application definition
